df8cf06bc06503e1e012c92b00bfcc73072258e8
Makavura <makavuramughanga@gmail.com>
2021-07-07 03:04:12 +0300

    feature: hide tree node section on click

--- src/app/surveys/surveys.component.ts
+++ src/app/surveys/surveys.component.ts
@@ -1,21 +1,46 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit,Renderer2,ViewChild } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { NgxSpinnerService } from "ngx-spinner";
+import { TreeProcessor } from '../../lib/nodes';
+import { TreeNodeRenderingService } from '../../lib/node.tree.services';
 
 @Component({
   selector: 'app-surveys',
   templateUrl: './surveys.component.html',
   styleUrls: ['./surveys.component.css']
 })
-export class SurveysComponent implements OnInit {
+export class SurveysComponent implements OnInit{
   forms: any;
   isLoading: Boolean;
   options: {};
+  nodeSections;
 
   constructor(
     private http: HttpClient,
-    private spinner: NgxSpinnerService
-  ) { }
+    private spinner: NgxSpinnerService,
+    private sanitizer: DomSanitizer,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {  }
+
+  @ViewChild('toggleButton') toggleButton: ElementRef;
+  @ViewChild('nodeSection') nodeSection: ElementRef;
+  @HostListener('document: click', ['$event'])
+  onGlobalClick(event: Event): void {
+    if(this.elementRef.nativeElement.contains(event.target)){
+      if(event.target instanceof Element) {
+        if((event.target as Element).classList.contains("tree-node-toggler")){
+          let treeNodeSections: HTMLCollectionOf<Element> = document.getElementsByClassName("tree-node-section"); 
+          for(const treeNode of treeNodeSections) {
+            if(treeNode.id === (event.target as Element).id) {
+              this.renderer.setStyle(treeNode, "display", "none");
+            }
+          }
+        }
+      }
+    }
+  }
 
   ngOnInit(): void {
     this.isLoading = true;
@@ -23,64 +48,12 @@ export class SurveysComponent implements OnInit {
     const headers = new HttpHeaders({ 'Authorization': "Token " });
     this.http.get(`http://fullstack-role.busara.io/api/v1/recruitment/forms/?node_type=Both`, { headers, observe: 'response' }).subscribe((response) => {
       this.forms = response.body["forms"];
-      console.log(this.forms)
+      let treeProcessor = new TreeProcessor(this.forms[0]["pages"]);
+      this.nodeSections = this.sanitizer.bypassSecurityTrustHtml(treeProcessor.renderTree());
       this.isLoading = false;
       this.spinner.hide();
     });
+
   }
 
-  onEvent = ($event) => console.log($event);
-}
-
-interface TreeNode  {
-  section: [ TreeNodeSection ]
-}
-
-interface TreeNodeSection {
-  created: string;
-  depth: number;
-  description: string;
-  name: string
-  questions: [ TreeNodeSectionQuestion ]
-}
-
-interface TreeNodeSectionQuestion {
-  column_match: string;
-  created: string;
-  description: string;
-  error_message: string;
-  text: string;
-  type: string;
-  is_visible: Boolean;
-  id: number;
-}
-
-/* 
-Tree View Survey Listing
-forms []
-  - pages []
-      - sections []
-          - questions []
-
-Requirements:
-- submission interface - ResponseSubmission
-*/
-
-interface ResponsesSubmission {
-  ans: [ ResponseSubmissionAns ];
-  end_time: string;
-  local_id: number;
-  location: {
-    accuracy: number,
-    lat: number,
-    lon: number
-  }
-  start_time: string;
-  survey_id: string;
-}
-
-interface ResponseSubmissionAns {
-  column_match: string;
-  q_ans: string;
-  q_id: string;
 }
