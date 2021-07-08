601c66c5b1df7e025a763c4f3978a5eb1daddae5
Makavura <makavuramughanga@gmail.com>
2021-07-08 23:29:14 +0300

    feature: class based survey submission tracking

--- src/lib/nodes.ts
+++ src/lib/nodes.ts
@@ -193,7 +193,7 @@ export class TreeProcessor {
             <div class="content  tree-node-section p-4 text-sm" id="${guid}"  style="display: none;" #questionSection>
 
                 <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button id="${guid}"  class="begin-survey inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <button id="${guid}" data-section-id="${section.id}" class="begin-survey inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Begin Survey
                     </button>
                 </div>
